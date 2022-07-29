03693e5423c6979cd23605527e881609da8168be
ADAMA KINDO <80959098+k-adama@users.noreply.github.com>
2022-07-29 15:55:45 +0000

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 7,
+  slidesPerGroup: 7,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
